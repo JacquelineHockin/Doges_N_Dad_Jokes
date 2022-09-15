f68c289b4accb165ef4040261702a45e580d0083
Jacqueline Cashman <jacquelinehcashman@gmail.com>
2022-09-14 23:27:56 -1000

    created 3 columns and scratched the speech-bubble idea, played with margins/padding/etc.

--- script.js
+++ script.js
@@ -109,15 +109,87 @@
 //   .then((response) => console.log(response))
 //   .catch((err) => console.error(err));
 
-const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "7a6ea40b65msh76b90858980b18ap120230jsn8cc36400d771",
-    "X-RapidAPI-Host": "dad-jokes.p.rapidapi.com",
-  },
-};
+// const options = {
+//   method: "GET",
+//   headers: {
+//     "X-RapidAPI-Key": "7a6ea40b65msh76b90858980b18ap120230jsn8cc36400d771",
+//     "X-RapidAPI-Host": "dad-jokes.p.rapidapi.com",
+//   },
+// };
 
-fetch("https://dad-jokes.p.rapidapi.com/random/joke", options)
-  .then((response) => response.json())
-  .then((response) => console.log(response))
-  .catch((err) => console.error(err));
+// fetch("https://dad-jokes.p.rapidapi.com/random/joke", options)
+//   .then((response) => response.json())
+//   .then((response) => console.log(response))
+//   .catch((err) => console.error(err));
+
+function fetchJokes() {
+  var url = "https://official-joke-api.appspot.com/random_joke";
+  fetch(url)
+    .then((response) => {
+      console.log("response", response);
+      if (response.ok) {
+        return response.text();
+      }
+      return response.text().then((err) => {
+        return Promise.reject({
+          status: response.status,
+          statusText: response.statusText,
+          errorMessage: err,
+        });
+      });
+    })
+    .then((data) => {
+      var setup = JSON.parse(data).setup;
+      document.getElementById("setup").innerHTML = setup;
+      var punchline = JSON.parse(data).punchline;
+      document.getElementById("punchline").innerHTML = punchline;
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
+function fetchDoge() {
+  var url = "http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=true";
+  fetch(url)
+    .then((response) => {
+      console.log("response", response);
+      if (response.ok) {
+        return response.text();
+      }
+      return response.text().then((err) => {
+        return Promise.reject({
+          status: response.status,
+          statusText: response.statusText,
+          errorMessage: err,
+        });
+      });
+    })
+    .then((data) => {
+      var dogeUrl = JSON.parse(data)[0];
+      var dogeImage = document.querySelector("#dogeImage");
+      dogeImage.style.backgroundImage = "url(" + dogeUrl + ")";
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
+var updatePage = function () {
+  fetchJokes();
+  fetchDoge();
+};
+var punchlineText = document.querySelector("#punchline");
+document.addEventListener("click", function (event) {
+  if (event.target === document.getElementById("showPunchline")) {
+    punchlineText.className = "show";
+  }
+});
+document.addEventListener("click", function (event) {
+  if (event.target === document.getElementById("refresh")) {
+    updatePage();
+    punchlineText.className = "hide";
+  }
+});
+var init = function () {
+  updatePage();
+};
+init();
